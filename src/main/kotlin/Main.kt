04c1bf3d1564a74df89899f16c0bbe54bbb2f1d8
CallumMcStay <151157058+CallumMcStay@users.noreply.github.com>
2024-11-15 10:34:48 +0000

    Add ten options in main and add userinput functions

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,21 @@
-package ie.setu
 
-fun main() {
-    println("Hello World!")
+import utils.readNextInt
+
+fun main() : Int {
+    println("""
+       / Movie App \
+       -------------
+      1) Add a movie
+      2) Add an actor
+      3) Assign an actor to a movie
+      4) List the movies
+      5) List the actors
+      6) Search for a movie
+      7) Search for an Actor
+      8) Show movie with specific actor
+      9) Show all actors in a specific movie
+      10) Show movies in certain genre
+    """.trimIndent())
+    return readNextInt(" > ==>>")
+
 }
